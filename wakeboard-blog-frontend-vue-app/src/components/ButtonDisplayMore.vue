e0b9e7496f16a21b0e0928c83c19069b4cef4b9d
michael <mickdu62200@gmail.com>
2021-05-02 16:14:31 +0200

    allow to provide the button 'display more' if number items above 15 articles

--- wakeboard-blog-frontend-vue-app/src/components/ButtonDisplayMore.vue
+++ wakeboard-blog-frontend-vue-app/src/components/ButtonDisplayMore.vue
@@ -1,27 +1,43 @@
-<div class="more_item">
-    <div class="container">
+<template>
+    <div class="more_item">
+        <div class="container">
 
-    <!--button to display more articles via ajax-->
-<div class="block_btn_more text-center col-xs-12">
-    <button id="btn-more" class="btn btn-warning"><i class="fa fa-angle-down"></i>Afficher plus</button>
-</div>
+        <!--button to display more articles via ajax-->
+        <div v-if="numberItems >= numberArticleLoad ">
+            <div class="block_btn_more text-center col-xs-12">
+                <button id="btn-more" class="btn btn-warning"><i class="fa fa-angle-down"></i>Afficher plus</button>
+            </div>
+        </div>
 
-<!--button to go back to the top of the page if there are more than 15 articles-->
-<div v-if="numberItems >= 15">
-    <div class="block_ancre_bottom text-right">
-    <a href="#title_block" class="ancre_bottom far fa-arrow-alt-circle-up fa-2x"></a>
+        <!--button to go back to the top of the page if there are more than 15 articles-->
+        <div v-if="numberItems >= 15">
+            <div class="block_ancre_bottom text-right">
+                <a href="#title_block" class="ancre_bottom far fa-arrow-alt-circle-up fa-2x"></a>
+            </div>
+        </div>
+        </div>
     </div>
-    </div>
-    </div>
-    </div>
+</template>
 
-    <script>
+<script>
 import { mapState } from 'vuex'
 
-export default {
-    name: 'ButtonDisplayMore',
-    computed: {
-        ...mapState(['numberItems'])
-    },
-}
+    export default {
+        name: 'ButtonDisplayMore',
+        computed: {
+            ...mapState(['numberItems'])
+        },
+        data() {
+            return {
+                numberArticleLoad: 10
+            }
+        },
+    }
 </script>
+
+<style lang="scss">
+    #btn-more
+    {
+        margin-bottom: 20px;
+    }
+</style>
