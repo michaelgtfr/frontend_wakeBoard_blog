1fc4764d744150b71940ac44116be098fd017855
michael <mickdu62200@gmail.com>
2021-05-02 19:39:41 +0200

    removal of the anchor part of the footer

--- wakeboard-blog-frontend-vue-app/src/components/ButtonDisplayMore.vue
+++ wakeboard-blog-frontend-vue-app/src/components/ButtonDisplayMore.vue
@@ -2,19 +2,13 @@
     <div class="more_item">
         <div class="container">
 
-        <!--button to display more articles via ajax-->
-        <div v-if="numberItems >= numberArticleLoad ">
-            <div class="block_btn_more text-center col-xs-12">
-                <button id="btn-more" class="btn btn-warning"><i class="fa fa-angle-down"></i>Afficher plus</button>
+            <!--button to display more articles via ajax-->
+            <div v-if="numberItems >= numberArticleLoad ">
+                <div class="block_btn_more text-center col-xs-12">
+                    <button id="btn-more" class="btn btn-warning"><i class="fa fa-angle-down"></i>Afficher plus</button>
+                </div>
             </div>
-        </div>
 
-        <!--button to go back to the top of the page if there are more than 15 articles-->
-        <div v-if="numberItems >= 15">
-            <div class="block_ancre_bottom text-right">
-                <a href="#title_block" class="ancre_bottom far fa-arrow-alt-circle-up fa-2x"></a>
-            </div>
-        </div>
         </div>
     </div>
 </template>
