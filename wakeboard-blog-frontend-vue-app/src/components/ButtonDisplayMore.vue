22bda16248113c53d1d65eef9e0d965694e6cf55
michael <mickdu62200@gmail.com>
2021-05-02 14:02:37 +0200

    contains the names of files not uploaded to git

---
+++ wakeboard-blog-frontend-vue-app/src/components/ButtonDisplayMore.vue
@@ -0,0 +1,27 @@
+<div class="more_item">
+    <div class="container">
+
+    <!--button to display more articles via ajax-->
+<div class="block_btn_more text-center col-xs-12">
+    <button id="btn-more" class="btn btn-warning"><i class="fa fa-angle-down"></i>Afficher plus</button>
+</div>
+
+<!--button to go back to the top of the page if there are more than 15 articles-->
+<div v-if="numberItems >= 15">
+    <div class="block_ancre_bottom text-right">
+    <a href="#title_block" class="ancre_bottom far fa-arrow-alt-circle-up fa-2x"></a>
+    </div>
+    </div>
+    </div>
+    </div>
+
+    <script>
+import { mapState } from 'vuex'
+
+export default {
+    name: 'ButtonDisplayMore',
+    computed: {
+        ...mapState(['numberItems'])
+    },
+}
+</script>
