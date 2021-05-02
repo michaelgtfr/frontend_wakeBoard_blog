f58be6cd683c1ed2b48e5aa2e521b3305aa33146
michael <mickdu62200@gmail.com>
2021-05-02 14:32:05 +0200

    contains the vuex library ans the fixtue data (global store)

---
+++ wakeboard-blog-frontend-vue-app/src/store/index.js
@@ -0,0 +1,61 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    numberItems: 6,
+    items: [
+        {
+          id: 1,
+          name: 'wakeboard-cable-park',
+          extension: 'jpg',
+          description: 'description1',
+          title: 'title1',
+        },
+        {
+          id: 2,
+          name: null,
+          extension: '',
+          description: '',
+          title: 'title2',
+        },
+        {
+            id: 3,
+            name: null,
+            extension: '',
+            description: '',
+            title: 'title3',
+        },
+        {
+            id: 4,
+            name: null,
+            extension: '',
+            description: '',
+            title: 'title4',
+        },
+        {
+            id: 5,
+            name: null,
+            extension: '',
+            description: '',
+            title: 'title5',
+        },
+        {
+            id: 6,
+            name: null,
+            extension: '',
+            description: '',
+            title: 'title6',
+        },
+      ],
+    user: { role_user: false },
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
