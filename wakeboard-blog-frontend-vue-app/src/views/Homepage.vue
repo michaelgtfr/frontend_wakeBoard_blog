11998a49240f925388bab02bb6580832288c88d3
michael <mickdu62200@gmail.com>
2021-05-02 14:33:32 +0200

    contains the view by routes

---
+++ wakeboard-blog-frontend-vue-app/src/views/Homepage.vue
@@ -0,0 +1,40 @@
+<template>
+    <section class="item">
+        <b-container>
+            <div class="row">
+                <div id="title_block" class="title_block col-lg-12">
+                    <h3 class="title_page">Les articles du sites:</h3>
+                </div>
+            </div>
+            <ListOfItem></ListOfItem>
+            <ButtonDisplayMore></ButtonDisplayMore>
+        </b-container>
+    </section>
+</template>
+
+<script>
+    import ListOfItem from '@/components/ListOfItem.vue'
+    import ButtonDisplayMore from '@/components/ButtonDisplayMore.vue'
+
+    export default {
+        name: 'Homepage',
+        components: {
+            ListOfItem,
+            ButtonDisplayMore
+        },
+    }
+</script>
+
+<style lang="scss">
+    .item
+    {
+        margin-bottom: 20px;
+    }
+
+    .title_page
+    {
+        text-align: center;
+        text-decoration: underline;
+        margin: 20px;
+    }
+</style>
