1bf0f38e0de371a3ffd8c244e491c5dfd403471a
michael <mickdu62200@gmail.com>
2021-05-05 09:46:32 +0200

    delect of the herder and ancre part

--- wakeboard-blog-frontend-vue-app/src/App.vue
+++ wakeboard-blog-frontend-vue-app/src/App.vue
@@ -22,22 +22,6 @@
                 </b-collapse>
             </b-container>
         </b-navbar>
-        <header class="masthead">
-            <div class="container d-flex h-100 align-items-center">
-                <div class="header_sentence mx-auto text-center">
-                    <h1 class="mx-auto my-0 text-uppercase">WakeTricks</h1>
-                    <h2 class="text-white-50 mx-auto mt-2 mb-5">Collaborative wake figure site</h2>
-                </div>
-            </div>
-            <div v-if="items !==[]">
-                <div class="container-fluid text-right">
-                    <div class="block_ancre">
-                        <a href="#title_block" class="ancre_header far fa-arrow-alt-circle-down fa-2x"></a>
-                    </div>
-                </div>
-            </div>
-        </header>
-
         <router-view/>
 
         <footer class="bg-black small text-center text-white-50">
